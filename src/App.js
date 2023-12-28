1535d1b7c761b67744738b5d51e691e3f44a85ff
soufhassani <souf.hassani@gmail.com>
2023-12-28 12:56:40 +0100

    Project done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const clickAnimation = (target) => {
+    const addBoxShadow = `inset 0 0 0px 40px rgba(255,255,255,0.2)`;
+    target.style.boxShadow = addBoxShadow;
+    setTimeout(() => (target.style.boxShadow = ""), 100);
+  };
+
+  const incrementHandler = (e) => {
+    e.preventDefault();
+    setCounter(counter + 1);
+    clickAnimation(e.currentTarget);
+  };
+
+  const decrementHandler = (e) => {
+    e.preventDefault();
+    setCounter(counter - 1);
+    clickAnimation(e.currentTarget);
+  };
+
   const buttons = {
-    btn1: {
+    incrementBtn: {
       id: "increment",
       text: "+",
-      callback: () => console.log("clicked increment"),
+      callback: incrementHandler,
     },
-    btn2: {
+    decrementBtn: {
       id: "decrement",
       text: "-",
-      callback: () => console.log("clicked decrement"),
+      callback: decrementHandler,
     },
   };
+
+  const addCounterClass = counter > 0 ? "red" : counter < 0 ? "blue" : "";
+
   return (
     <main>
+      <div className="content-container">
+        <h1 className={addCounterClass}>{counter}</h1>
+      </div>
       <div className="buttons-container">
-        <Button {...buttons.btn1} />
-        <Button {...buttons.btn2} />
+        <Button {...buttons.decrementBtn} />
+        <Button {...buttons.incrementBtn} />
       </div>
     </main>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,12 +2,9 @@ import React from "react";
 
 const Button = (props) => {
   const { id, text, callback } = props;
-  const clickHandler = (e) => {
-    e.preventDefault();
-    callback();
-  };
+  
   return (
-    <button type="button" id={id} onClick={clickHandler}>
+    <button type="button" id={id} onClick={callback}>
       {text}
     </button>
   );
